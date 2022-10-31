ade7b6d770b2c4355a2986df3b2d6fd275efc033
Serhii Svitlychnyi <94105673+SergSvet87@users.noreply.github.com>
2022-10-31 09:43:46 +0200

    Added files webpack, styles, scripts

---
+++ main.js
@@ -0,0 +1,14 @@
+/*--------------------------------- 
+Main JavaScript 
+-----------------------------*/
+
+// import swiper from "./modules/swiper";
+// import slick from "./modules/slick";
+// import tabs from "./modules/tabs";
+import mobileMenu from "./modules/mobileMenu";
+
+// swiper();
+// slick();
+// tabs();
+mobileMenu();
+
